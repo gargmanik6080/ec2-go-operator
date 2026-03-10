6754c74c003a4e266009c010d38cef45254647d9
gargmanik6080 <gargmanik6080@gmail.com>
2026-03-10 18:26:24 +0530

    adding aws client helper

---
+++ internal/controller/aws_client.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"context"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
+
+func awsClient(region string) *ec2.Client {
+	// read creds from env
+	accessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region), config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyID, secretAccessKey, "")))
+
+	if err != nil {
+		return nil
+	}
+
+	return ec2.NewFromConfig(cfg)
+}
